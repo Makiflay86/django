131e316154bbaafff8313eda0f7ecb6b13b1237e
Francisco Aybar <frannur1.ir@gmail.com>
2026-01-19 19:36:31 +0100

    tmb añadiendo los libros

--- HolaMundo/views.py
+++ HolaMundo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse # Necesario para poder responder al cliente
-from HolaMundo.models import Author
+from HolaMundo.models import Author,Book
 
 # Create your views here.
 
@@ -10,5 +10,6 @@ def hola_mundo (request): # El request captura las peticiones de los clientes
 # Con esto hemos habilitado una vista, pero hay que además enlazar el proyecto con la aplicación
 
 def home (request): # Pinta una página con render, también hay que darlo de alta en urls.py
-    author = Author.objects.all() # Añadiendo los autores al index
-    return render(request,'index.html', {'authors': author}) # La página index.html hay que crearla dentro del archivo de configuración de todo proyecto de django, settings.py
+    author = Author.objects.all() # Añadiendo los autores
+    book = Book.objects.all() # Añadiendo los libros
+    return render(request,'index.html', {'authors': author, 'books': book}) # La página index.html hay que crearla dentro del archivo de configuración de todo proyecto de django, settings.py
