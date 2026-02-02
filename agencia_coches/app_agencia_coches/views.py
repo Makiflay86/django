8e55bb508d545939adab59da27b98856b021ed2f
Francisco Aybar <frannur1.ir@gmail.com>
2026-02-02 21:13:32 +0100

    ya se ve en las view, como home

--- agencia_coches/app_agencia_coches/views.py
+++ agencia_coches/app_agencia_coches/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def hola_mundo (request):
     return HttpResponse ("<h1>hola mundo</h1>")
 
 def home (request):
-    return render(request,'index.html')
+    cars = Cars.objects.all()
+    return render(request,'index.html',{'cars':cars})
