5ca5610fce1b6c24b516d3a861d9593c3084e454
Francisco Aybar <frannur1.ir@gmail.com>
2026-02-02 20:26:29 +0100

    tablas de la bd

--- agencia_coches/agencia_coches/urls.py
+++ agencia_coches/agencia_coches/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
+# Activamos el soporte de imágenes
+from django.conf import settings 
+from django.conf.urls.static import static
+
 from app_agencia_coches import views # Importamos todas las vistas de nuestra app
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +30,6 @@ urlpatterns = [
     path('hola_mundo/', views.hola_mundo),
     path('home/', views.home),
 ]
+
+
+if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- agencia_coches/app_agencia_coches/models.py
+++ agencia_coches/app_agencia_coches/models.py
@@ -1,3 +1,157 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+# Tabla empleado
+class Employee (models.Model):
+
+    # Nombre
+    nombre = models.CharField (
+        verbose_name = 'Nombre', 
+        max_length = 100,
+        default = ''
+    )
+
+    # Apellidos
+    apellidos = models.CharField (
+        verbose_name = 'Apellidos', 
+        max_length = 100,
+        default = ''
+    )
+
+    # Puesto de trabajo (ej: vendedor, gerente, etc.)
+    puesto = models.CharField (
+        verbose_name = 'Puesto', 
+        max_length = 100,
+        default = ''
+    )
+
+    # Fecha de contratación
+    fecha_contratacion = models.DateField (
+        verbose_name = 'Fecha de contratacion', 
+        auto_now_add = True
+    )
+
+    # Foto del empleado
+    foto = models.ImageField (
+        verbose_name = 'Foto',
+        upload_to = "empleados/",
+        blank = True, 
+        null = True
+    )
+
+    def __str__(self):
+        return self.nombre + " " + self.apellidos + " | " + self.puesto       
+
+
+
+# Tabla extra (ej: techo solar)
+class Extra(models.Model):
+
+    nombre = models.CharField(
+        max_length = 100,
+        default = ''
+    )
+
+    def __str__(self):
+        return self.nombre
+
+
+
+# Tabla coches
+class Cars (models.Model):
+
+    # Marca (ej: Audi)
+    marca = models.CharField (
+        verbose_name = 'Marca', 
+        max_length = 100,
+        default = ''
+    )
+
+    # Modelo (ej: A5)
+    modelo = models.CharField (
+        verbose_name = 'Modelo', 
+        max_length =  100,
+        default = ''
+    )
+    
+    # Fecha de fabricación
+    fecha_fabricacion = models.DateField (
+        verbose_name = 'Fecha de fabricacion', 
+        default = date.today
+    )
+
+    # Kilómetros
+    kilometros = models.PositiveIntegerField (
+        verbose_name = 'Kilometros', 
+        default = 0
+    )
+
+    # Combustible
+    combustible = models.CharField (
+        verbose_name = 'Combustible', 
+        max_length = 100,
+        default = ''
+    )
+
+    # Color
+    color = models.CharField (
+        verbose_name = 'Color', 
+        max_length = 100,
+        default = ''
+    )
+
+    # Imágen portada
+    imagen = models.ImageField (
+        verbose_name = 'Imagen',
+        upload_to = "coches/",
+        blank=True,
+        null=True
+    )
+
+    # Fecha de la última modificación
+    fecha_modificacion = models.DateField (
+        auto_now = True
+    )
+
+    creado_por = models.ForeignKey(
+        Employee, 
+        verbose_name = 'Creado por', 
+        on_delete = models.CASCADE
+    ) 
+
+    extras = models.ManyToManyField( 
+        Extra,
+        verbose_name = 'Extras',
+        related_name = 'coches', 
+        blank=True 
+    )
+
+
+    def __str__(self):
+        return self.marca + " " + self.modelo
+
+
+
+# El resto de imágenes de los coches
+class CarImages(models.Model):
+
+    # Relación con la tabla Cars
+    car = models.ForeignKey (
+        Cars, 
+        on_delete = models.CASCADE, 
+        related_name = 'imagenes_adicionales'
+    )
+
+    # Imagen adicional
+    imagen = models.ImageField(
+        verbose_name = 'Imagen Adicional',
+        upload_to = "coches/galeria/",
+        default = "",
+        blank = True,
+        null = True
+    )
+
+    def __str__(self):
+        return f"Imagen de {self.car.marca} {self.car.modelo}"
+
