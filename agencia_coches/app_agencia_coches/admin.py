4916beb645d03fd699678154ed1cc9e22140df8e
Francisco Aybar <frannur1.ir@gmail.com>
2026-02-02 20:29:16 +0100

    importar en admin.py los models

--- agencia_coches/app_agencia_coches/admin.py
+++ agencia_coches/app_agencia_coches/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# Importamos todos los models
+from .models import Employee, Extra, Cars, CarImages
+
+admin.site.register(Employee)
+admin.site.register(Extra)
+admin.site.register(Cars)
+admin.site.register(CarImages)
