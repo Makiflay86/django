df2ced9d161f17052894077ef96282edde2bd616
Francisco Aybar <frannur1.ir@gmail.com>
2026-02-19 15:01:15 +0100

    casi implementado el carrusel de imagenes

--- agencia_coches/app_agencia_coches/admin.py
+++ agencia_coches/app_agencia_coches/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import *
 
-# Importamos todos los models
-from .models import Employee, Extra, Cars, CarImages
 
+
+# Registros sencillos
 admin.site.register(Employee)
 admin.site.register(Extra)
-admin.site.register(Cars)
-admin.site.register(CarImages)
+
+
+
+# 1. El Inline para las fotos adicionales
+class CarImagesInline(admin.TabularInline):
+    model = CarImages
+    extra = 10
+    fields = ['imagen']
+
+
+
+# 2. La configuración de Cars
+@admin.register(Cars)
+class CarsAdmin(admin.ModelAdmin):
+    list_display = ('ver_miniatura', 'marca', 'modelo', 'combustible', 'kilometros')
+    inlines = [CarImagesInline]
+
+    def ver_miniatura(self, obj):
+        if obj.imagen:
+            return format_html('<img src="{}" style="width: 50px; height: 35px; border-radius: 5px; object-fit: cover;" />', obj.imagen.url)
+        return "Sin foto"
+    
+    ver_miniatura.short_description = 'Vista Previa'
